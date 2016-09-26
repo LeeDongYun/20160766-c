bc11945d80755ece87906beee37ddfcd023ae6e1
LeeDongYun <sora619@naver.com>
2016-09-27 02:27:46 +0900

    Delete Putstring.c

--- Putstring.c
+++
@@ -1,5 +0,0 @@
-﻿1>------ 빌드 시작: 프로젝트: First C Project, 구성: Debug Win32 ------
-1>  Putstring.c
-1>  First C Project.vcxproj -> c:\users\user\documents\visual studio 2015\Projects\Cho2\Debug\First C Project.exe
-1>  First C Project.vcxproj -> c:\users\user\documents\visual studio 2015\Projects\Cho2\Debug\First C Project.pdb (Full PDB)
-========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========
